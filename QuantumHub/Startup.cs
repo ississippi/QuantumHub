d7bf933a82c632d103127175f5af4a36b4528941
Gary Smith <ississippi@outlook.com>
2021-08-24 09:34:56 -0400

    Enable SwaggerUI

--- QuantumHub/Startup.cs
+++ QuantumHub/Startup.cs
@@ -60,14 +60,14 @@ namespace QuantumHub
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
+            //if (env.IsDevelopment())
+            //{
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "QuantumHub v1"));
-            }
+            //}
             // Enable middleware to serve generated Swagger as a JSON endpoint.
-            app.UseSwagger();
+            //app.UseSwagger();
 
             app.UseCors(x => x
                 .AllowAnyMethod()
